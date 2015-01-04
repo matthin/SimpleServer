e631ec903f1bdc729aa7fc626fd56b983d003095
Justin Harrison <justin@pyrohail.com>
2015-01-04 11:42:43 -0500

    Display server listen failure

--- Source/Server.cc
+++ Source/Server.cc
@@ -5,12 +5,18 @@
 #include "Config.hh"
 #include <memory>
 #include <thread>
+#include <iostream>
 
 ss::Server::Server(const unsigned short port) : listener()
 {
 	Config config;
 
 	listener.listen(port);
+	if (listener.get_local_port() == 0)
+	{
+		std::cout << "Listener could not attach port: " << port << std::endl;
+		exit(1);
+	}
 
 	while (true) {
 		Socket* socket = new Socket;
